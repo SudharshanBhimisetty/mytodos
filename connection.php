227e6ae963f6de1721b3077e7e153cf5e47c5b13
SudharshanBhimisetty <sudharshanbhimisetty24@gmail.com>
2020-11-18 19:30:39 +0530

    database is changed to app.infinity.fee

--- connection.php
+++ connection.php
@@ -1,17 +1,7 @@
 <?php
-// $con = mysqli_connect("localhost","root","root");
 
-/* $con = mysqli_connect("sql12.freemysqlhosting.net","sql12368562","98YISBJKN6");
-
-mysqli_select_db($con, "sql12368562");
-
-?>
- */
-
-
-$con = mysqli_connect("sql12.freemysqlhosting.net","sql12370913","p4sRvRzEmj");
-
-mysqli_select_db($con, "sql12370913");
+$con = mysqli_connect("sql110.epizy.com","epiz_27226830","SAuLrQSNDTab");
+mysqli_select_db($con, "epiz_27226830_phptodosapp"); 
 
 ?>
 
